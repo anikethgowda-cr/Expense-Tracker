e1345cc8a000a468dbd8ff1e04a0caaee5344cb4
aniketh <anikethgowda2611@gmail.com>
2026-09-21 13:03:16 +0530

    Add backend as normal project folder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,14 @@
+import dns from "dns";
 import mongoose from "mongoose";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || process.env.MONGODB_URI);
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || process.env.MONGODB_URI
+    );
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
